d7a6f1dce2fb244ca8aba6680589938f0af9a166
joshe <JosheTheys@gmail.com>
2022-06-07 10:02:20 +0200

    first

---
+++ script/code.js
@@ -0,0 +1,71 @@
+// 1. Please create an array called bestFriends with your friend names
+// 2. Display the middle and last names Atleast 10
+
+// const names = [
+//     {
+//       firstName: "",
+//       middleName: "",
+//       lastName: "",
+//     },
+//     {
+//         firstName: "",
+//         middleName: "",
+//         lastName: "",
+//     },
+//     {
+//         firstName: "",
+//         middleName: "",
+//         lastName: "",
+//     },
+//     {
+//         firstName: "",
+//         middleName: "",
+//         lastName: "",
+//     },
+//     {
+//         firstName: "",
+//         middleName: "",
+//         lastName: "",
+//     },
+//     {
+//         firstName: "",
+//         middleName: "",
+//         lastName: "",
+//     },
+//     {
+//         firstName: "",
+//         middleName: "",
+//         lastName: "",
+//     },
+//     {
+//         firstName: "",
+//         middleName: "",
+//         lastName: "",
+//     },
+//     {
+//         firstName: "",
+//         middleName: "",
+//         lastName: "",
+//     },
+//     {
+//         firstName: "",
+//         middleName: "",
+//         lastName: "",
+//     },
+//   ];
+//   console.log(names)
+  
+  
+//    const nameContainer = document.getElementById("names");
+//   names.forEach((nameContainer) => {
+//     nameContainer.innerHTML += `
+//       <h3>${name.middleName}</h3>
+//       <h3>${name.lastName}</h3>
+//     `;
+//   });
+
+const names = ["Amanda", "Xena", "Mikayla", "Meaghan", "Cassidy", "Reagan", "Marshalino", "Mikhail", "Cameron","Monique"];
+console.log(names);
+
+console.log(names[4]);
+console.log(names[9]);
